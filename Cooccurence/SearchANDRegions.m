166c1c4db5b8e00d2de294d5534481ad73873959
xyan14 <qxyyxq@gmail.com>
2017-07-12 15:52:47 +0200

    regular updates

--- Cooccurence/SearchANDRegions.m
+++ Cooccurence/SearchANDRegions.m
@@ -1,9 +1,18 @@
 % search regions where all specified genes co-occur
 % Xiaoyan, 2017
 
-[name, pos] = getinsitudata('C:\Users\qxyyx\Desktop\temp\beforeQT_details.csv');
+decoded_file = 'Decoding\QT_0.45_0_details.csv';
+image = 'slideA_alignedHE.jpg';
+scale = 1;
 
-cooccur = search_reads_cooccur(name, pos, 100);
-cooccur = search_reads_cooccur(name, pos, 100, {'Drd3', 'Prrx1', 'Ctdnep1'});
+%%
+[name, pos] = getinsitudata(decoded_file);
+[name, pos] = removereads(name, 'NNNN', pos);
+figure;
+plotall(name, pos, image, scale)
 
+
+% cooccur = search_reads_cooccur(name, pos, 100);
+% cooccur = search_reads_cooccur(name, pos, 100, {'Drd3', 'Prrx1', 'Ctdnep1'});
+cooccur = search_reads_combs(name, pos, 100, 3, {'Drd3', 'Prrx1'});
 
