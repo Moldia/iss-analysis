46002664bea6e0eb2592a7554d5d0bc5972e8beb
xyanqian <xiaoyan.qian@scilifelab.se>
2017-09-25 17:42:59 +0200

    read cufflinks fpkm files and write simplified csv

--- RNAseq/SimplifyFPKMfiles.py
+++ RNAseq/SimplifyFPKMfiles.py
@@ -1,18 +1,36 @@
 import os
+from openpyxl import load_workbook
+
+
+# all genes that we have probes against
+allgenes = []
+
+wb = load_workbook(filename=r"C:\Users\Xiaoyan\OneDrive\worky\allProbes.xlsm", read_only=True)
+ws = wb['Sheet1']
+
+for row in ws.rows:
+    allgenes.append(row[4].value)
+
+allgenes = list(set(allgenes))
+allgenes.remove("name")
+
 
 for s in range(25, 29):
     for l in range(4):
-        filename = os.path.join(r"E:\RNAseq\170825_NB501365_0108_AHJ2HNBGX3\fpkm",
-                                "S" + str(s) + "_L00" + str(l+1),
-                                "genes.fpkm_tracking")
-        with open(filename, "r") as f:
-            for line in f:
-                line = line.rstrip('\n')
-                items = line.split('\t')
-                if items[-1] == "OK":
-                    geneid = items[3]
-                    genename = items[4]
-                    fpkm = items[9]
+        outfilename = os.path.join(r"G:\RNAseq\170825_NB501365_0108_AHJ2HNBGX3\UngappedMapping\fpkm",
+                                   "S" + str(s) + "_L00" + str(l+1),
+                                   "fpkm_sipmle.csv")
 
+        with open(outfilename, 'w') as fout:
+
+            filename = os.path.join(r"G:\RNAseq\170825_NB501365_0108_AHJ2HNBGX3\UngappedMapping\fpkm",
+                                    "S" + str(s) + "_L00" + str(l+1),
+                                    "genes.fpkm_tracking")
+            with open(filename, 'r') as fin:
+                for line in fin:
+                    line = line.rstrip('\n')
+                    items = line.split('\t')
+                    if items[-1] == "OK" and items[4] in allgenes:
+                        fout.write("%s,%s,%f\n" % (items[3], items[4], float(items[9])))
 
 
