1e7a92b6d15305501e82fbd174a72ee957f81b81
Xiaoyan Qian <xiaoyan.qian@scilifelab.se>
2019-01-21 14:38:51 +0100

    plot nan when showing only legend to prevent entries being grayed out

--- lib/visualization/show_only_legend.m
+++ lib/visualization/show_only_legend.m
@@ -1,18 +1,23 @@
-function show_only_legend(ah, legendnames, symbols)
+function show_only_legend(ah, legendnames, symbols, fontsize)
 % show_only_legend(ah, legendnames, symbols)
 % create an invisible dummy figure in subplot to show only legend
-% Xiaoyan, 2017
+% Xiaoyan, 2019
 
 
-randomPoints = rand(length(legendnames),2);
+% randomPoints = rand(length(legendnames),2);
 
 axes(ah);
 hold on;
 for i = 1:numel(legendnames)
-    ph = plot(randomPoints(i,1), randomPoints(i,2), symbols{i});
-    set(ph, 'visible', 'off');
+    ph = plot(nan, nan, symbols{i});
+%     set(ph, 'visible', 'off');
 end
-h = legend(legendnames, 'color', [.6 .6 .6], 'fontsize', 5);
+if nargin > 3
+    h = legend(legendnames, 'color', [.6 .6 .6], 'fontsize', fontsize);
+else
+    h = legend(legendnames, 'color', [.6 .6 .6], 'fontsize', 5);    
+end
 set(h, 'location', 'west');
 set(ah, 'visible', 'off');
+
 end
